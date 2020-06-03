9e623e119b064ac9ef4f2c3aa24ad60f03f51e46
danielparz <dparzynski@gmail.com>
2020-06-03 11:58:15 +0200

    Dodaj pliki projektów.

---
+++ Fibonacci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci
+{
+    static class Fibonacci
+    {
+        public static int GetElement(int n)
+        {
+            if (n == 1) return 0;
+            if (n <= 3) return 1;
+            else return GetElement(n - 1) + GetElement(n - 2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ilość wyrazów ciągu Fibonacciego do wypisania: ");
+            Int32.TryParse(Console.ReadLine(), out int n);
+
+            Console.WriteLine($"{n} pierwszych wyrazów ciągu Fibonacciego: ");
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"{Fibonacci.GetElement(i)} ");
+            }
+
+        }
+    }
+}
